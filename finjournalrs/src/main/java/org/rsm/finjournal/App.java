dd29b20f5d182453523461f41f1e92a33fa986ed
rsm82 <rao.mrs@gmail.com>
2017-05-04 17:44:52 -0400

    src and pom.xml added

---
+++ finjournalrs/src/main/java/org/rsm/finjournal/App.java
@@ -0,0 +1,13 @@
+package org.rsm.finjournal;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
